86065c123854b25eedb46a800bdaa5c7abbdb237
Mateusz Misztoft <mateusz.misztoft@gmail.com>
2017-09-05 07:01:03 +0200

    update animation

--- src/game.js
+++ src/game.js
@@ -1,3 +1,11 @@
+window.requestAnimFrame = (function(callback) {
+  return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+  function(callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
+})();
+
+
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -358,7 +366,7 @@ function Game() {
   };
 
   this.loop = function() {
-    window.requestAnimationFrame(this.loop);
+    window.requestAnimFrame(this.loop);
     
     currentTime = (new Date()).getTime();
 
